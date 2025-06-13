959908e25d685c7213474ae1e0b947fc18ec5cf1
Dip Coder <mymailforlaptop@protonmail.com>
2025-06-13 03:00:14 +0000

    feat: complete periodic table element lookup script

--- element.sh
+++ element.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+# Check if no argument
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
+fi
+
+# Check if argument is atomic number (a number)
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1")
+else
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = INITCAP('$1') OR name = INITCAP('$1')")
+fi
+
+# If element not found
+if [[ -z $ELEMENT ]]; then
+  echo "I could not find that element in the database."
+  exit
+else
+  IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING BOILING <<< "$ELEMENT"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
 fi
